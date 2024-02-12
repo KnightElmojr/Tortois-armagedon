57a51443983d2ae6d1303f5210aeedf5c658ff25
Artem Rudyk <rudykartem44@gmail.com>
2024-02-12 13:11:58 +0200

    added rotation v0.1

--- scripts/Character.cs
+++ scripts/Character.cs
@@ -3,37 +3,60 @@ using System;
 
 public partial class Character : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	private const float _speed = 100.0f;
+	private const float _jumpSpeed = -400.0f;
+	private const float RotationSpeed = 3f;
+	private const float Deceleration = 0.5f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	// Get the gravity from the project settings so you can sync with rigid body nodes.
+	public float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
 		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+		velocity.Y += Gravity * (float)delta;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+		// Handle jump.
+		if (Input.IsActionJustPressed("ui_up") && IsOnFloor())
+			velocity.Y = _jumpSpeed;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		// Get the input direction.
+		float direction = Input.GetAxis("ui_left", "ui_right");
+		velocity.X = direction * _speed;
+
+		// Get the input direction for rotating
+		
+		bool rotateLeft = Input.IsActionPressed("ui_rotate_left");
+		bool rotateRight = Input.IsActionPressed("ui_rotate_right");
+
+		if (rotateLeft)
 		{
-			velocity.X = direction.X * Speed;
+			Rotation -= RotationSpeed * (float)delta;
+		}
+
+		if (rotateRight)
+		{
+			Rotation += RotationSpeed * (float)delta;
+		}
+		if (rotateLeft || rotateRight)
+		{
+			// Apply forward movement based on rotation
+			velocity = new Vector2(0, 0).Rotated(Rotation) * _speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			// Decelerate if no rotation keys are pressed
+			velocity = velocity.Lerp(Vector2.Zero, Deceleration * (float)delta);
 		}
-
+		
 		Velocity = velocity;
 		MoveAndSlide();
+		
 	}
+
+	
 }
